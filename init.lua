c46e62973ef03fe9f98e9be78d152f184f237194
Eklavya Mirani <github1@eklavyamirani.com>
2025-05-18 10:09:25 -0700

    configured nvim may 18 25

--- init.lua
+++ init.lua
@@ -5,7 +5,18 @@ vim.o.number = true  -- Show line numbers
 vim.o.relativenumber = false -- Use relative line numbers
 vim.g.mapleader = ' '    -- Set space as leader key
 vim.g.maplocalleader = ' '  -- Optional: Set space as local leader key (for buffer-local mappings)
-vim.opt.undofile = true
+vim.o.undofile = true
+vim.o.clipboard = "unnamedplus"
+vim.o.title = true
+vim.o.tabstop = 2
+vim.o.ruler = false
+vim.o.showmode = false
+vim.o.showcmd = false
+vim.o.numberwidth = 4
+vim.o.smarttab = true
+vim.o.expandtab = true -- we use space instead of tab around here.
+vim.o.shiftwidth = 2
+vim.o.tabstop = 8 -- identify tabs easily.
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -20,4 +31,5 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 
 -- keymaps --
-vim.api.nvim_set_keymap('n', '<leader>e', ':lua MiniFiles.open()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>e', ':lua MiniFiles.open()<CR>', { noremap = trdf;ue, silent = true })
+vim.keymap.set('n',"<leader>t", ":NvimTreeToggle<CR>") -- open nvim tree file explorer
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,12 +1,73 @@
 local vim = vim
+local Plug = vim.fn['plug#']
 
--- Load vim-plug
-vim.cmd [[
-  call plug#begin('~/.local/share/nvim/plugged')
+vim.call('plug#begin')
 
-  " Add your plugins here
-  " Plug 'echasnovski/mini.nvim'
-  call plug#end()
-]]
+Plug('nvim-lualine/lualine.nvim') --statusline
+Plug('nvim-tree/nvim-web-devicons')
+Plug('echasnovski/mini.nvim') -- file explorer
+Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) --colorscheme
+Plug('uZer/pywal16.nvim', { [ 'as' ] = 'pywal16' }) --or, pywal colorscheme
+Plug('folke/which-key.nvim') --mappings popup
+Plug('nvim-treesitter/nvim-treesitter') --improved syntax
+Plug('mfussenegger/nvim-lint') --async linter
+Plug('nvim-tree/nvim-tree.lua') --file explorer
+Plug('windwp/nvim-autopairs') --autopairs 
+Plug('lewis6991/gitsigns.nvim') --git
+Plug('numToStr/Comment.nvim') --easier comments
+Plug('norcalli/nvim-colorizer.lua') --color highlight
 
--- require('mini.files').setup()
+vim.call('plug#end')
+
+-- setup plugins
+require('mini.files').setup()
+require('nvim-tree').setup({
+  sync_root_with_cwd = true,
+  update_cwd = true,
+})
+require('nvim-autopairs').setup()
+require('lualine').setup()
+require('gitsigns').setup()
+require("Comment").setup({
+  ---Add a space b/w comment and the line
+  padding = true,
+  ---Whether the cursor should stay at its position
+  sticky = true,
+  ---Lines to be ignored while (un)comment
+  ignore = nil,
+  ---LHS of toggle mappings in NORMAL mode
+  toggler = {
+    ---Line-comment toggle keymap
+    line = "gcc",
+    ---Block-comment toggle keymap
+    block = "gb",
+  },
+  -- -LHS of operator-pending mappings in NORMAL and VISUAL mode
+  opleader = {
+    ---Line-comment keymap
+    line = "gcc",
+    ---Block-comment keymap
+    block = "gb",
+  },
+  ---LHS of extra mappings
+  extra = {
+    ---Add comment on the line above
+    above = "gcO",
+    ---Add comment on the line below
+    below = "gco",
+    ---Add comment at the end of line
+    eol = "gcA",
+  },
+  ---Enable keybindings
+  ---NOTE: If given `false` then the plugin won't create any mappings
+  mappings = {
+    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+    basic = true,
+    ---Extra mapping; `gco`, `gcO`, `gcA`
+    extra = true,
+  },
+  ---Function to call before (un)comment
+  pre_hook = nil,
+  ---Function to call after (un)comment
+  post_hook = nil,
+})
