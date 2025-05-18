9f60fb1962b2cb847869702ee7b15c7f7ea745fa
Eklavya Mirani <github1@eklavyamirani.com>
2025-05-18 10:18:06 -0700

    added command to auto install vim plug

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+-- auto install vim-plug and plugins, if not found
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+  vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+  vim.o.runtimepath = vim.o.runtimepath
+  vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
 require('config.plugins')
 
 -- Basic settings -- 
