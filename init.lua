8640e702760b9fa8585ae0055920088cfe387373
Eklavya Mirani <github1@eklavyamirani.com>
2026-04-27 23:25:13 -0700

    migrate config to use nvim 0.12 vim.pack

--- init.lua
+++ init.lua
@@ -1,60 +1,56 @@
--- auto install vim-plug and plugins, if not found
-local data_dir = vim.fn.stdpath('data')
 local config_dir = vim.fn.stdpath('config')
-if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
-  local vim_plug_autoload_cmd = 'silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  local confirmation_message = "This will install vim plug into auto load:\n" .. vim_plug_autoload_cmd
-  local confirmation = vim.fn.confirm(confirmation_message, "&Yes\n&No", 2)
-  if (confirmation == 1) then
-    vim.cmd(vim_plug_autoload_cmd)
-    -- @todo_eky: what was the context of this line?
-    vim.o.runtimepath = vim.o.runtimepath
-    vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
-  end
-end
 
--- Optimized module loader, improves startup performance
-vim.loader.enable();
+-- Optimized module loader, improves startup performance.
+vim.loader.enable()
+vim.o.termguicolors = true  -- must be set before colorscheme
 require('config.plugins')
-local notify = require('mini.notify')
--- @todo_eky wrap into autocmd
-if (vim.fn.trim(vim.fn.system('git -C ' .. config_dir .. ' pull --dry-run')) ~= '') then
-  local notificationId = notify.add('Remote config updated', 'WARN', 'Comment')
-  vim.defer_fn(function() MiniNotify.remove(notificationId) end, 1000)
-end
 
--- Basic settings -- 
-vim.o.number = true  -- Show line numbers
-vim.o.relativenumber = true -- Use relative line numbers
-vim.g.mapleader = ' '    -- Set space as leader key
-vim.g.maplocalleader = ' '  -- Optional: Set space as local leader key (for buffer-local mappings)
+local notify = require('mini.notify')
+-- Async check for remote config updates so startup isn't blocked by the network.
+vim.api.nvim_create_autocmd('VimEnter', {
+  once = true,
+  callback = function()
+    vim.system(
+      { 'git', '-C', config_dir, 'pull', '--dry-run' },
+      { text = true },
+      function(out)
+        if (out.stdout or '') ~= '' or (out.stderr or '') ~= '' then
+          vim.schedule(function()
+            local notificationId = notify.add('Remote config updated', 'WARN', 'Comment')
+            vim.defer_fn(function() notify.remove(notificationId) end, 1000)
+          end)
+        end
+      end
+    )
+  end,
+})
+
+-- Basic settings --
+vim.o.number = true            -- Show line numbers
+vim.o.relativenumber = true    -- Use relative line numbers
+vim.g.mapleader = ' '          -- Set space as leader key
+vim.g.maplocalleader = ' '     -- Local leader (for buffer-local mappings)
 vim.o.undofile = true
-vim.o.clipboard = "unnamedplus"
+vim.o.clipboard = 'unnamedplus'
 vim.o.title = true
-vim.o.tabstop = 2
 vim.o.ruler = false
 vim.o.showmode = false
 vim.o.showcmd = false
 vim.o.numberwidth = 4
 vim.o.smarttab = true
-vim.o.expandtab = true -- we use space instead of tab around here.
+vim.o.expandtab = true         -- spaces, not tabs
 vim.o.shiftwidth = 2
-vim.o.tabstop = 8 -- identify tabs easily.
+vim.o.tabstop = 8              -- make stray tabs visually obvious
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+-- See `:help 'list'` and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Show which line your cursor is on
 vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
 -- keymaps --
-vim.api.nvim_set_keymap('n', '<leader>ed', '<Cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>', { noremap = trdf;ue, silent = true })
-vim.keymap.set('n', '<leader>ei', '<Cmd>edit $MYVIMRC<CR>', { desc = 'init.lua'})
--- disable. trying out mini.files instead
--- vim.keymap.set('n',"<leader>t", ":NvimTreeToggle<CR>") -- open nvim tree file explorer
+vim.keymap.set('n', '<leader>ed', function()
+  MiniFiles.open(vim.api.nvim_buf_get_name(0))
+end, { desc = 'mini.files' })
+vim.keymap.set('n', '<leader>ei', '<Cmd>edit $MYVIMRC<CR>', { desc = 'init.lua' })
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,78 +1,121 @@
-local vim = vim
-local Plug = vim.fn['plug#']
+-- Plugin management with the built-in vim.pack (Neovim 0.12+).
+-- Plugins are cloned to ~/.local/share/nvim/site/pack/core/opt/<name>
+-- on first launch (synchronously) and loaded automatically.
+vim.pack.add({
+  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+  { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+  { src = 'https://github.com/nvim-mini/mini.files',    version = 'stable' },
+  { src = 'https://github.com/nvim-mini/mini.notify',   version = 'stable' },
+  { src = 'https://github.com/catppuccin/nvim',         name = 'catppuccin' },
+  { src = 'https://github.com/folke/which-key.nvim' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/mfussenegger/nvim-lint' },
+  { src = 'https://github.com/windwp/nvim-autopairs' },
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  { src = 'https://github.com/numToStr/Comment.nvim' },
+  { src = 'https://github.com/catgoose/nvim-colorizer.lua' },
+  { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/eklavyamirani/code-review.nvim' },
+  { src = 'https://github.com/ibhagwan/fzf-lua' },
+})
 
-vim.call('plug#begin')
+-- Helper for plugins whose only setup is `require('x').setup()`.
+local function setup(name, opts)
+  local ok, mod = pcall(require, name)
+  if not ok then
+    vim.notify('Failed to load ' .. name, vim.log.levels.WARN)
+    return
+  end
+  if type(mod.setup) == 'function' then mod.setup(opts or {}) end
+end
 
-Plug('nvim-lualine/lualine.nvim') --statusline
-Plug('nvim-tree/nvim-web-devicons')
-Plug('nvim-mini/mini.files', { ['branch'] = 'stable'} ) -- file explorer
-Plug('nvim-mini/mini.diff', { ['branch'] = 'stable'} ) -- diff helper
-Plug('nvim-mini/mini.notify', { ['branch'] = 'stable'} ) -- notifications
-Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) --colorscheme
-Plug('folke/which-key.nvim') --mappings popup
-Plug('nvim-treesitter/nvim-treesitter') --improved syntax
-Plug('mfussenegger/nvim-lint') --async linter
--- disabling, trying out mini files instead
--- Plug('nvim-tree/nvim-tree.lua') --file explorer
-Plug('windwp/nvim-autopairs') --autopairs 
-Plug('lewis6991/gitsigns.nvim') --git
-Plug('numToStr/Comment.nvim') --easier comments
-Plug('norcalli/nvim-colorizer.lua') --color highlight
-Plug('MeanderingProgrammer/render-markdown.nvim') --render md inline
+setup('mini.files')
+setup('mini.notify')
+setup('nvim-autopairs')
+setup('lualine')
+setup('which-key')
+setup('colorizer')
+setup('render-markdown')
+setup('code-review')
+setup('fzf-lua')
 
-vim.call('plug#end')
+-- Treesitter: highlight + indent for installed parsers.
+local ok_ts, ts = pcall(require, 'nvim-treesitter.configs')
+if ok_ts then
+  ts.setup({
+    ensure_installed = { 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline', 'bash', 'json', 'yaml' },
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  })
+end
 
--- setup plugins
-require('mini.files').setup()
-require('mini.diff').setup()
--- require('nvim-tree').setup({
---   sync_root_with_cwd = true,
---   update_cwd = true,
--- })
-require('mini.notify').setup()
-require('nvim-autopairs').setup()
-require('lualine').setup()
--- require('gitsigns').setup()
-require("Comment").setup({
-  ---Add a space b/w comment and the line
+-- Colorscheme.
+pcall(vim.cmd.colorscheme, 'catppuccin')
+
+-- gitsigns: set buffer-local hunk-navigation/staging keymaps when attached.
+setup('gitsigns', {
+  on_attach = function(buf)
+    local gs = require('gitsigns')
+    local map = function(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, { buffer = buf, desc = desc })
+    end
+    map(']c', function() gs.nav_hunk('next') end, 'Next hunk')
+    map('[c', function() gs.nav_hunk('prev') end, 'Prev hunk')
+    map('<leader>hp', gs.preview_hunk, 'Preview hunk')
+    map('<leader>hs', gs.stage_hunk,   'Stage hunk')
+    map('<leader>hr', gs.reset_hunk,   'Reset hunk')
+    map('<leader>hb', function() gs.blame_line({ full = true }) end, 'Blame line')
+    map('<leader>hd', gs.diffthis, 'Diff this')
+  end,
+})
+
+setup('Comment', {
   padding = true,
-  ---Whether the cursor should stay at its position
   sticky = true,
-  ---Lines to be ignored while (un)comment
   ignore = nil,
-  ---LHS of toggle mappings in NORMAL mode
   toggler = {
-    ---Line-comment toggle keymap
-    line = "gcc",
-    ---Block-comment toggle keymap
-    block = "gb",
+    line = 'gcc',
+    block = 'gbc',
   },
-  -- -LHS of operator-pending mappings in NORMAL and VISUAL mode
+  -- opleader is for operator-pending motions, e.g. `gcap` to comment a paragraph.
   opleader = {
-    ---Line-comment keymap
-    line = "gcc",
-    ---Block-comment keymap
-    block = "gb",
+    line = 'gc',
+    block = 'gb',
   },
-  ---LHS of extra mappings
   extra = {
-    ---Add comment on the line above
-    above = "gcO",
-    ---Add comment on the line below
-    below = "gco",
-    ---Add comment at the end of line
-    eol = "gcA",
+    above = 'gcO',
+    below = 'gco',
+    eol = 'gcA',
   },
-  ---Enable keybindings
-  ---NOTE: If given `false` then the plugin won't create any mappings
   mappings = {
-    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
     basic = true,
-    ---Extra mapping; `gco`, `gcO`, `gcA`
     extra = true,
   },
-  ---Function to call before (un)comment
-  pre_hook = nil,
-  ---Function to call after (un)comment
-  post_hook = nil,
 })
+
+-- nvim-lint: configure linters per filetype and trigger on save / read.
+local ok_lint, lint = pcall(require, 'lint')
+if ok_lint then
+  lint.linters_by_ft = {
+    lua = { 'luacheck' },
+    sh = { 'shellcheck' },
+    bash = { 'shellcheck' },
+    markdown = { 'markdownlint' },
+  }
+  vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
+    callback = function() lint.try_lint() end,
+  })
+end
+
+-- fzf-lua keymaps.
+local ok_fzf, fzf = pcall(require, 'fzf-lua')
+if ok_fzf then
+  vim.keymap.set('n', '<leader>ff', fzf.files,         { desc = 'Find files' })
+  vim.keymap.set('n', '<leader>fg', fzf.live_grep,     { desc = 'Live grep' })
+  vim.keymap.set('n', '<leader>fb', fzf.buffers,       { desc = 'Buffers' })
+  vim.keymap.set('n', '<leader>fh', fzf.help_tags,     { desc = 'Help tags' })
+  vim.keymap.set('n', '<leader>fr', fzf.resume,        { desc = 'Resume last picker' })
+  vim.keymap.set('n', '<leader>f/', fzf.lgrep_curbuf,  { desc = 'Grep current buffer' })
+end
